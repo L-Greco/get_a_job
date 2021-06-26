88dda7fe7b4159b4c5642e7a9ab351bdfbe64c5d
L-Greco <makaronaskonstandinos@gmail.com>
2021-06-26 03:03:19 +0300

    push

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
-export const likeCompany = (company) => ({
-    type: "LIKE_COMPANY",
-    payload: company
-})
+export const likeCompany = (company) => {
+    return { type: "LIKE_COMPANY", payload: company }
+}
 
 export const unlikeCompany = (company) => ({
     type: "UNLIKE_COMPANY",
