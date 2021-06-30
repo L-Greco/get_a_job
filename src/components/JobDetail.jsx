bd6e60ce662e0d72a532e960f91bc1d56a086fe0
L-Greco <makaronaskonstandinos@gmail.com>
2021-06-30 10:59:45 +0300

    clear some noise

--- src/components/JobDetail.jsx
+++ src/components/JobDetail.jsx
@@ -1,16 +1,8 @@
 const JobDetail = ({ job, title }) => {
   return (
-    <main
-      id="jobDetailPage"
-      // style={{
-      //   position: "fixed",
-      //   top: "100px",
-      //   overflow: "auto",
-      //   height: "80vh",
-      // }}
-    >
+    <main id="jobDetailPage">
       <h2>{title} </h2>
-      {job.toLowerCase().includes("<script>") ? (
+      {job.toLowerCase().includes("<script") ? (
         alert("DANGER , SCRIPT INSIDE THE HTML!!")
       ) : (
         <td dangerouslySetInnerHTML={{ __html: job }} />
